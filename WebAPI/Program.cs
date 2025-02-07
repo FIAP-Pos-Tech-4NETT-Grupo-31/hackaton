c64ff7c161cd77acbd9702a3797758fe73874260
Paulo Abinair <paulon0@yahoo.com.br>
2025-02-06 22:42:56 -0300

    fix: inicialização do swagger tinha sido removida

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -100,8 +100,8 @@ var app = builder.Build();
 
 //if (app.Environment.IsDevelopment())
 //{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 //}
 
 
